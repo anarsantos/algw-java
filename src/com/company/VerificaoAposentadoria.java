65d813effeea28355dbbb0fd7898e4bb80b59e52
ana <aninha_rsantos@yahoo.com.br>
2023-02-03 15:09:57 -0300

    subida DescontoFrete e GastosFamiliar

---
+++ src/com/company/VerificaoAposentadoria.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+/*Crie um programa que receba a idade de uma pessoa e a quantidade de tempo que essa pessoa contribuiu
+ com a previdência, depois verifique se ela pode aposentar ou não.
+ Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos e ter contribuído com,
+ pelo menos, 25 anos. Se ela passar nessas duas condições, então mostre pra ela
+ uma mensagem dizendo que ela pode aposentar.*/
+
+
+public class VerificaoAposentadoria {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+    }
+}
